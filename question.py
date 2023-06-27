32f7397648e7c485ca7dc57bf70bf4a69b59b605
Michael A <67983421+PsyChrome-G3@users.noreply.github.com>
2023-06-27 11:58:10 +0100

    Implemented error handling in the QuestionBank class.

--- question.py
+++ question.py
@@ -22,18 +22,25 @@ class QuestionBank:
         self.questions.append(question)
 
     def load_from_json(self, json_file):
-        with open(json_file) as file:
-            question_bank_data = json.load(file)
+        try:
+            with open(json_file) as file:
+                question_bank_data = json.load(file)
 
-        for question_data in question_bank_data:
-            question_id = question_data['question_id']
-            question_text = question_data['question_text']
-            screenshot_path = question_data['screenshot_path']
-            answer_choices = question_data['answer_choices']
-            correct_answer = question_data['correct_answer']
+            for question_data in question_bank_data:
+                question_id = question_data['question_id']
+                question_text = question_data['question_text']
+                screenshot_path = question_data['screenshot_path']
+                answer_choices = question_data['answer_choices']
+                correct_answer = question_data['correct_answer']
 
-            question = Question(question_id, question_text, screenshot_path, answer_choices, correct_answer)
-            self.questions.append(question)
+                question = Question(question_id, question_text, screenshot_path, answer_choices, correct_answer)
+                self.questions.append(question)
+
+        except FileNotFoundError:
+            print(f"Error: JSON file '{json_file}' not found.")
+
+        except json.JSONDecodeError:
+            print(f"Error: Invalid JSON format in file '{json_file}'.")
 
     def get_question_by_id(self, question_id):
         for question in self.questions:
@@ -45,5 +52,21 @@ class QuestionBank:
         return random.choice(self.questions)
 
     def save_to_json(self, json_file):
-        # Save self.questions to a JSON file
-        pass
+        try:
+            question_bank_data = []
+            for question in self.questions:
+                question_data = {
+                    'question_id': question.qid,
+                    'question_text': question.question,
+                    'screenshot_path': question.screenshot_path,
+                    'answer_choices': question.answers,
+                    'correct_answer': question.correct_answer
+                }
+                question_bank_data.append(question_data)
+
+            with open(json_file, 'w') as file:
+                json.dump(question_bank_data, file)
+
+        except Exception as e:
+            print(f"Error: Failed to save question bank to JSON file '{json_file}'.")
+            print(e)
