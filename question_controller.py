0b152d311ce521d03a413cae9613b405e7874de6
Michael A <67983421+PsyChrome-G3@users.noreply.github.com>
2023-06-27 11:47:40 +0100

    Moved template rendering to a class in template_renderer.py.

--- question_controller.py
+++ question_controller.py
@@ -1,5 +1,6 @@
-from flask import render_template, request
-from markupsafe import Markup
+from flask import request
+from template_renderer import TemplateRenderer
+
 
 class QuestionController:
     def __init__(self, question_bank):
@@ -8,17 +9,9 @@ class QuestionController:
     def get_random_question(self):
         return self.question_bank.get_random_question()
 
-    def render_question(self, template_name):
-        question = self.get_random_question()
-        question_text = question.question
-        answer_choices = question.answers
-        question_id = question.qid
-        screenshot_path = question.screenshot_path
-        return render_template(template_name, question_text=question_text, answer_choices=answer_choices,
-                               question_id=question_id, screenshot_path=screenshot_path)
-
     def home(self):
-        return self.render_question('index.html')
+        question = self.get_random_question()
+        return TemplateRenderer.render_question('index.html', question)
 
     def result(self):
         question_id = request.form['question_id']
@@ -27,19 +20,11 @@ class QuestionController:
         question = self.question_bank.get_question_by_id(question_id)
 
         if question is None:
-            return render_template('error.html', message='Question not found')
+            return TemplateRenderer.render('error.html', message='Question not found')
 
-        correct_answer = question.correct_answer
-
-        if selected_answer == correct_answer:
-            result = Markup('<div class="alert alert-success" role="alert">Congratulations! You answered correctly.</div>')
-        else:
-            incorrect_alert = Markup('<div class="alert alert-danger" role="alert">Sorry, your answer is incorrect.</div>')
-            correct_alert = Markup('<div class="alert alert-primary" role="alert"><strong>The correct answer is:</strong><br>'
-                                   '<div class="correct-answer">' + correct_answer + '</div></div>')
-            result = incorrect_alert + correct_alert
-
-        return render_template('result.html', result=result)
+        correct_answer = str(question.correct_answer)  # Convert to string
+        return TemplateRenderer.render_result(correct_answer, selected_answer)
 
     def new_question(self):
-        return self.render_question('index.html')
+        question = self.get_random_question()
+        return TemplateRenderer.render_question('index.html', question)
---
+++ template_renderer.py
@@ -0,0 +1,31 @@
+from flask import render_template
+from markupsafe import Markup
+
+
+class TemplateRenderer:
+    @staticmethod
+    def render(template_name, **context):
+        return render_template(template_name, **context)
+
+    @staticmethod
+    def render_question(template_name, question):
+        context = {
+            'question_text': question.question,
+            'answer_choices': question.answers,
+            'question_id': question.qid,
+            'screenshot_path': question.screenshot_path
+        }
+        return TemplateRenderer.render(template_name, **context)
+
+    @staticmethod
+    def render_result(correct_answer, selected_answer):
+        if selected_answer == correct_answer:
+            result = Markup('<div class="alert alert-success" role="alert">Congratulations! You answered correctly.</div>')
+        else:
+            incorrect_alert = Markup('<div class="alert alert-danger" role="alert">Sorry, your answer is incorrect.</div>')
+            correct_alert = Markup(
+                '<div class="alert alert-primary" role="alert"><strong>The correct answer is:</strong><br>'
+                '<div class="correct-answer">' + correct_answer + '</div></div>')
+            result = incorrect_alert + correct_alert
+
+        return TemplateRenderer.render('result.html', result=result)
